ed86960de47bf6f4cd917216112d0141f4b1291b
1260110411 <1260110411@qq.com>
2019-11-24 23:11:30 +0800

    序列化与反序列化

---
+++ Test/SoapFormatter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace SoapFormatter
+{
+    /// <summary>
+    /// SoapFormatter的序列化方式
+    /// </summary>
+    [Serializable]
+    //如果要想保存某个class中的字段,必须在class前面加个这样attribute(C#里面用中括号括起来的标志符)
+    public class Person
+    {
+        public int age;
+        public string name;
+        [NonSerialized] //如果某个字段不想被保存,则加个这样的标志
+        public string secret;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///序列化:
+            Person person = new Person();
+            person.age = 18;
+            person.name = "tom";
+            person.secret = "i will not tell you";
+            FileStream stream = new FileStream(@"C:\Users\Public\Desktop\person.xml", FileMode.Create);
+            System.Runtime.Serialization.Formatters.Soap.SoapFormatter bFormat = 
+            new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
+            //SoapFormatter bFormat = new SoapFormatter();
+            bFormat.Serialize(stream, person);
+            stream.Close();
+
+            ///反序列化
+            Person person1 = new Person();
+            FileStream stream1 = new FileStream(@"C:\Users\Public\Desktop\person.xml", FileMode.Open);
+            System.Runtime.Serialization.Formatters.Soap.SoapFormatter bFormat1 =
+            new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
+            //SoapFormatter bFormat1 = new SoapFormatter();
+            person1 = (Person)bFormat.Deserialize(stream1);
+            //反序列化得到的是一个object对象.必须做下类型转换
+            stream1.Close();
+            Console.WriteLine(person1.age + person1.name + person1.secret);
+            //结果为18tom.因为secret没有有被序列化.
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test/XmlSerializer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace XmlSerializer
+{
+    /// <summary>
+    /// XmlSerializer的序列化方式
+    /// </summary>
+    [Serializable]
+    //如果要想保存某个class中的字段,必须在class前面加个这样attribute(C#里面用中括号括起来的标志符)
+    public class Person
+    {
+        public int age;
+        public string name;
+        [NonSerialized] //如果某个字段不想被保存,则加个这样的标志
+        public string secret;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///序列化:
+            Person person = new Person();
+            person.age = 18;
+            person.name = "tom";
+            person.secret = "i will not tell you";
+            FileStream stream = new FileStream(@"C:\Users\Public\Desktop\xmlFormat.xml", FileMode.Create);
+            System.Xml.Serialization.XmlSerializer xmlserilize = new System.Xml.Serialization.XmlSerializer(typeof(Person));
+            //XmlSerializer xmlserilize = new XmlSerializer(typeof(Person));
+            xmlserilize.Serialize(stream, person);
+            stream.Close();
+            ///反序列化:
+            Person person1 = new Person();
+            FileStream stream1 = new FileStream(@"C:\Users\Public\Desktop\xmlFormat.xml", FileMode.Open);
+            System.Xml.Serialization.XmlSerializer xmlserilize1 = new System.Xml.Serialization.XmlSerializer(typeof(Person));
+            person1 = (Person)xmlserilize.Deserialize(stream1);
+            stream1.Close();
+            Console.WriteLine(person1.age + person1.name + person1.secret);
+
+            Console.ReadKey();
+        }
+    }
+}
